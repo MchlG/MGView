3714cb1c26b080a5bbaa28695b3d7ed89c0bdc9a
MchlG <gordonmzx@gmail.com>
2020-06-05 12:11:58 -0400

    add MGButton

---
+++ Sources/MGView/Button.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mike on 6/5/20.
+//
+
+import UIKit
+
+class MGButton: UIButton {
+    
+    var touchUp: (()-> Void)?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setActions()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            UIView.animate(withDuration: 0.15, delay: 0.0, options: [.beginFromCurrentState, .allowUserInteraction], animations: {
+                self.alpha = self.isHighlighted ? 0.25 : 1
+            }, completion: nil)
+        }
+    }
+    
+    private func setActions() {
+        self.addTarget(self, action: #selector(touchUpInside), for: .touchUpInside)
+    }
+    
+    @objc private func touchUpInside() {
+        self.touchUp?()
+    }
+    
+}
+
